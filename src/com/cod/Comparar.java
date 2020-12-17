bd6b65429390ed2807bdf8c10fe49e31e4e93eb1
Cristian Blanco Fidalgo <cblancofidalgo@danielcastelao.org>
2020-12-17 19:28:51 +0100

    .jar

--- src/com/cod/Comparar.java
+++ src/com/cod/Comparar.java
@@ -3,22 +3,6 @@ package com.cod;
 public class Comparar {
     String gmail = "";
     String ip = "127.0.0.2";
-    private static Comparar instance = null;
-    
-    public static Comparar gentInstance(){
-        if (instance == null){
-            instance = new Comparar();
-        }
-        gmail = "anonymous@danielcastelao.org";
-        return instance;
-    }
-
-    public static Comparar getInstance(String gmail){
-        if (instance == null){
-            instance = new Comparar();
-        }
-
-    }
 
     Comparar() {
         gmail = "anonymous@danielcastelao.org";
